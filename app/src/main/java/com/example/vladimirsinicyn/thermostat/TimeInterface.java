2651c15d697edcd8ebe2de06849b3ef5e0d74357
vladimirsinicyn <oldgrezli@mail.ru>
2015-05-28 12:38:59 +0300

    Add interface for timer

---
+++ app/src/main/java/com/example/vladimirsinicyn/thermostat/TimeInterface.java
@@ -0,0 +1,17 @@
+package com.example.vladimirsinicyn.thermostat;
+
+//interface for class Time
+
+public interface TimeInterface {
+
+    //increment minutes, simply minutes++
+    void incrementTime();
+
+    //return true if hours == 0 and minutes == 0
+    boolean checkMidnight();
+
+    //return time in string like "12:00"
+    String toString();
+}
+
+
