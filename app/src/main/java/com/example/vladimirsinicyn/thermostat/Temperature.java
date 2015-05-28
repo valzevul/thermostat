5b903aea3af021ed255cb197e2a6cafc7c8d86d0
vladimirsinicyn <oldgrezli@mail.ru>
2015-05-28 14:22:00 +0300

    Temperature updated

--- app/src/main/java/com/example/vladimirsinicyn/thermostat/Temperature.java
+++ app/src/main/java/com/example/vladimirsinicyn/thermostat/Temperature.java
@@ -13,7 +13,17 @@ public class Temperature implements Serializable {
         fractionalPart = 0;
     }
 
-    public Temperature(int integer, int fractional) {
+    public Temperature(int integer, int fractional) throws IllegalArgumentException {
+
+        if (fractional >= 10 || fractional < 0) {
+            throw new IllegalArgumentException("Temperature(int integer, int fractional): Incorrect fractional" +
+                    " part of the temperature.");
+        }
+
+        if (integer < 0) {
+            throw new IllegalArgumentException("Temperature(int integer, int fractional): Incorrect integer" +
+                    " part of the temperature.");
+        }
 
         this.integerPart = integer;
         this.fractionalPart = fractional;
@@ -35,7 +45,22 @@ public class Temperature implements Serializable {
         if (fractionalPart == -1) {
 
             integerPart--;
-            fractionalPart = 0;
+            fractionalPart = 9;
         }
+    }
+
+    public String toString() {
+
+        return integerPart + "." + fractionalPart;
+    }
+
+    public int getIntegerPart() {
+
+        return integerPart;
+    }
+
+    public int getFractionalPart() {
+
+        return fractionalPart;
     }
 }
